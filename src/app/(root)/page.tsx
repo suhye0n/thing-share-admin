1d3d06d0e45e77e919b5c741cf351e5dabc33e16
suhyeon <contact.suhyeon@gmail.com>
2025-06-12 21:34:58 +0900

    feat: StoreModal에 폼 및 zod 기반 유효성 검증 추가

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { UserButton } from '@clerk/nextjs';
+import { Modal } from '@/components/ui/modal';
 import { useStoreModal } from '@/hooks/use-store-modal';
 import { useEffect } from 'react';
 
 const Page = () => {
   const onOpen = useStoreModal((state) => state.onOpen);
   const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) onOpen();
+  }, [isOpen, onOpen]);
 
   return (
     <div className="p-4">
--- src/components/modals/store-modal.tsx
+++ src/components/modals/store-modal.tsx
@@ -1,16 +1,73 @@
 'use client';
 
+import * as z from 'zod';
+
 import { Modal } from '@/components/ui/modal';
 import { useStoreModal } from '@/hooks/use-store-modal';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModal();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
   return (
     <Modal
       title="Test"
       description="Test Desc"
       isOpen={storeModal.isOpen}
       onClose={storeModal.onClose}
-    />
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Test" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end">
+                <Button variant="outline" onClick={storeModal.onClose}>
+                  취소
+                </Button>
+                <Button type="submit">완료</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
   );
 };
